672d76c6cf9e8e27bc039547db8f065adc5a5c39
Jason <jasonxiii97@gmail.com>
2023-07-09 14:23:10 +0200

    Add a script for the player robot input

---
+++ Assets/Game/Scripts/Stage/PlayerRobotInput.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerRobotInput : MonoBehaviour
+{
+	public Vector2 MovementVector {get; private set;}
+
+	private void OnMove(InputValue iv)
+	{
+		Vector2 movement = iv.Get<Vector2>();
+
+		MovementVector = movement;
+
+		Debug.Log(MovementVector);
+	}
+}
