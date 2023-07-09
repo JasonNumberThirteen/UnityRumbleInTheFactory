11ee7ec5de0aed54bafc7535d71ba02c57d45945
Jason <jasonxiii97@gmail.com>
2023-07-09 13:20:01 +0200

    Add connection between a counter and counter text

--- Assets/Game/Scripts/Counter.cs
+++ Assets/Game/Scripts/Counter.cs
@@ -3,12 +3,38 @@ using UnityEngine;
 public class Counter : MonoBehaviour
 {
 	public int initialValue;
+	public CounterText text;
 
 	public int CurrentValue {get; private set;}
 
-	public void SetTo(int value) => CurrentValue = value;
-	public void IncreaseBy(int value) => CurrentValue += value;
-	public void DecreaseBy(int value) => CurrentValue -= value;
+	public void SetTo(int value)
+	{
+		CurrentValue = value;
+
+		UpdateText();
+	}
+
+	public void IncreaseBy(int value)
+	{
+		CurrentValue += value;
+
+		UpdateText();
+	}
+
+	public void DecreaseBy(int value)
+	{
+		CurrentValue -= value;
+
+		UpdateText();
+	}
 
 	private void Start() => SetTo(initialValue);
+
+	private void UpdateText()
+	{
+		if(text != null)
+		{
+			text.UpdateText();
+		}
+	}
 }
