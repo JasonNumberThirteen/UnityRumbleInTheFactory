e8f5239db601caedfa79146ba259ad1eaf3081f2
Jason <jasonxiii97@gmail.com>
2023-07-10 13:34:27 +0200

    Add the splatter effect to bullets after collision

--- Assets/Game/Scripts/Stage/Bullet/BulletCollider.cs
+++ Assets/Game/Scripts/Stage/Bullet/BulletCollider.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class BulletCollider : MonoBehaviour
 {
+	public GameObject splatterEffect;
+	
 	private void OnTriggerEnter2D(Collider2D collider)
 	{
+		Instantiate(splatterEffect, gameObject.transform.position, Quaternion.identity);
 		Destroy(gameObject);
 	}
 }
