aee37c38f5753ab1941c4b665dc20002152914da
Jason <jasonxiii97@gmail.com>
2023-07-10 14:45:57 +0200

    Add excluded tags for bullet collisions

--- Assets/Game/Scripts/Stage/Bullet/BulletCollider.cs
+++ Assets/Game/Scripts/Stage/Bullet/BulletCollider.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class BulletCollider : MonoBehaviour
 {
 	public GameObject splatterEffect;
+	public string[] excludedTags;
 	
 	private void OnTriggerEnter2D(Collider2D collider)
 	{
+		foreach (string et in excludedTags)
+		{
+			if(collider.CompareTag(et))
+			{
+				return;
+			}
+		}
+		
 		Instantiate(splatterEffect, gameObject.transform.position, Quaternion.identity);
 		Destroy(gameObject);
 	}
