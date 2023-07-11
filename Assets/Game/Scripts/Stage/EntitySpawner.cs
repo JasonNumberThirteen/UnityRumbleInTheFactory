ed28f916ce758836d6cbacd007643740f91f7476
Jason <jasonxiii97@gmail.com>
2023-07-11 18:42:25 +0200

    Automatically assign spawned entities to the object

--- Assets/Game/Scripts/Stage/EntitySpawner.cs
+++ Assets/Game/Scripts/Stage/EntitySpawner.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class EntitySpawner : MonoBehaviour
 {
 	public GameObject entity;
-	public Transform parent;
 
 	public void Spawn()
 	{
 		GameObject instance = Instantiate(entity, gameObject.transform.position, Quaternion.identity);
 
-		instance.transform.SetParent(parent);
+		instance.transform.SetParent(GameObject.FindGameObjectWithTag("Entities").transform);
 	}
 }
