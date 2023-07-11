3a2f6a767e6574f96cabb232ca4a20c48ada307d
Jason <jasonxiii97@gmail.com>
2023-07-11 18:50:58 +0200

    Fix enemy robots detecting each other

--- Assets/Game/Scripts/Stage/Enemy Robot/EnemyRobotCollider.cs
+++ Assets/Game/Scripts/Stage/Enemy Robot/EnemyRobotCollider.cs
@@ -5,12 +5,4 @@ public class EnemyRobotCollider : MonoBehaviour
 	private EnemyRobotMovement movement;
 
 	private void Awake() => movement = GetComponent<EnemyRobotMovement>();
-
-	private void OnCollisionEnter2D(Collision2D collider)
-	{
-		if(collider.gameObject.CompareTag(gameObject.tag))
-		{
-			movement.RandomiseDirection();
-		}
-	}
 }
