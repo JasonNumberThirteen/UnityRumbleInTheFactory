b62283c4ab60740f557599f088755161fc8f5466
Jason <jasonxiii97@gmail.com>
2023-07-11 19:35:56 +0200

    Add a variable for setting tag of the parent of spawned entities

--- Assets/Game/Scripts/Scenes/Stage/EntitySpawner.cs
+++ Assets/Game/Scripts/Scenes/Stage/EntitySpawner.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class EntitySpawner : MonoBehaviour
 {
 	public GameObject entity;
+	public string parentTag;
 
 	public void Spawn()
 	{
 		GameObject instance = Instantiate(entity, gameObject.transform.position, Quaternion.identity);
 
-		instance.transform.SetParent(GameObject.FindGameObjectWithTag("Entities").transform);
+		instance.transform.SetParent(GameObject.FindGameObjectWithTag(parentTag).transform);
 	}
 }
