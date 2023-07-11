a0a4ca12f24cc80f021b8c72030cc284ede44634
Jason <jasonxiii97@gmail.com>
2023-07-11 17:28:18 +0200

    Make collision detection by the enemy robot using raycast

--- Assets/Game/Scripts/Stage/Enemy Robot/EnemyRobotCollider.cs
+++ Assets/Game/Scripts/Stage/Enemy Robot/EnemyRobotCollider.cs
@@ -5,9 +5,4 @@ public class EnemyRobotCollider : MonoBehaviour
 	private EnemyRobotMovement movement;
 
 	private void Awake() => movement = GetComponent<EnemyRobotMovement>();
-	
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-		movement.RandomiseDirection();
-	}
 }
--- Assets/Game/Scripts/Stage/Enemy Robot/EnemyRobotMovement.cs
+++ Assets/Game/Scripts/Stage/Enemy Robot/EnemyRobotMovement.cs
@@ -4,11 +4,35 @@ using UnityEngine;
 public class EnemyRobotMovement : MonoBehaviour
 {
 	[Min(0f)] public float movementSpeed;
+	[Min(0.01f)] public float rayDistance = 0.5f;
+	public LayerMask raycastExcludedLayers;
 	
 	private Rigidbody2D rb2D;
 	private Vector2 direction;
+	
+	public Vector2 MovementVector() => direction;
 
-	public void RandomiseDirection()
+	private void Awake() => rb2D = GetComponent<Rigidbody2D>();
+	private void Start() => RandomiseDirection();
+
+	private void FixedUpdate()
+	{
+		float speed = movementSpeed*Time.fixedDeltaTime;
+		Vector2 rayPosition = direction*rayDistance;
+
+		rb2D.MovePosition(rb2D.position + direction*speed);
+
+		RaycastHit2D hit = Physics2D.Raycast(rb2D.position, direction, rayDistance, ~raycastExcludedLayers);
+
+		if(hit.collider != null)
+		{
+			RandomiseDirection();
+		}
+
+		Debug.DrawLine(rb2D.position, rb2D.position + rayPosition, Color.red);
+	}
+
+	private void RandomiseDirection()
 	{
 		float randomValue = Random.value;
 		Vector2 currentDirection = direction;
@@ -34,17 +58,5 @@ public class EnemyRobotMovement : MonoBehaviour
 		{
 			RandomiseDirection();
 		}
-	}
-
-	public Vector2 MovementVector() => direction;
-
-	private void Awake() => rb2D = GetComponent<Rigidbody2D>();
-	private void Start() => RandomiseDirection();
-
-	private void FixedUpdate()
-	{
-		float speed = movementSpeed*Time.fixedDeltaTime;
-
-		rb2D.MovePosition(rb2D.position + direction*speed);
 	}
 }
