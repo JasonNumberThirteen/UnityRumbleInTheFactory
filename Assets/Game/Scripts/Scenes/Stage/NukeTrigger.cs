4f206369cbf7166a644b52fd0d2cd278625f9e94
Jason <jasonxiii97@gmail.com>
2023-07-13 14:17:56 +0200

    Add a script for the nuke trigger

---
+++ Assets/Game/Scripts/Scenes/Stage/NukeTrigger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class NukeTrigger : MonoBehaviour, ITriggerable
+{
+	public void TriggerEffect()
+	{
+		NukeRenderer nr = GetComponent<NukeRenderer>();
+		EntityExploder ee = GetComponent<EntityExploder>();
+
+		if(nr != null)
+		{
+			nr.ChangeToDestroyedState();
+			Destroy(nr);
+		}
+		
+		if(ee != null)
+		{
+			ee.Explode();
+		}
+	}
+}
