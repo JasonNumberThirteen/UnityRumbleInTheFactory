bba701e7f9aa62f31a61e1e11461762697cf95bc
Jason <jasonxiii97@gmail.com>
2023-07-13 12:11:17 +0200

    Add metal tiles around the nuke as a part of the fortress bonus effect

--- Assets/Game/Scripts/Scenes/Stage/Bonus/FortressBonusEffect.cs
+++ Assets/Game/Scripts/Scenes/Stage/Bonus/FortressBonusEffect.cs
@@ -4,6 +4,7 @@ public class FortressBonusEffect : BonusEffect
 {
 	[Min(0f)] public float overlapBoxSize = 2.5f;
 	public LayerMask overlapLayers;
+	public GameObject metalTile;
 	
 	public override void PerformEffect()
 	{
@@ -12,8 +13,21 @@ public class FortressBonusEffect : BonusEffect
 
 		foreach (Collider2D collider in colliders)
 		{
-			Debug.Log(collider.gameObject.name);
 			Destroy(collider.gameObject);
+		}
+
+		for (int y = 0; y <= 3; ++y)
+		{
+			for (int x = 0; x <= 5; ++x)
+			{
+				if(x <= 1 || x >= 4 || y >= 2)
+				{
+					Vector2 topLeftPosition = new Vector2(-1.75f, -6.25f);
+					Vector2 offset = new Vector2(x, y)*0.5f;
+					
+					Instantiate(metalTile, topLeftPosition + offset, Quaternion.identity);
+				}
+			}
 		}
 	}
 }
