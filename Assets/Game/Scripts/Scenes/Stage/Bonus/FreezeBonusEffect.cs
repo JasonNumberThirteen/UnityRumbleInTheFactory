2c97bedfb878d52e0feeded570b7c56954c178f3
Jason <jasonxiii97@gmail.com>
2023-07-13 10:30:18 +0200

    Edit the freeze bonus effect script

--- Assets/Game/Scripts/Scenes/Stage/Bonus/FreezeBonusEffect.cs
+++ Assets/Game/Scripts/Scenes/Stage/Bonus/FreezeBonusEffect.cs
@@ -1,32 +1,18 @@
 using UnityEngine;
-using System.Collections;
 
 public class FreezeBonusEffect : BonusEffect
 {
 	[Min(0.01f)] public float duration = 10f;
 	
-	public override void PerformEffect() => StartCoroutine(FreezeEnemies());
-	
-	private IEnumerator FreezeEnemies()
+	public override void PerformEffect()
 	{
 		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
 		
 		foreach (GameObject enemy in enemies)
 		{
-			EnemyRobotMovement erm = enemy.GetComponent<EnemyRobotMovement>();
-			EnemyRobotShoot ers = enemy.GetComponent<EnemyRobotShoot>();
+			EnemyRobotFreeze erf = enemy.GetComponent<EnemyRobotFreeze>();
 
-			erm.enabled = ers.enabled = false;
-		}
-
-		yield return new WaitForSeconds(duration);
-
-		foreach (GameObject enemy in enemies)
-		{
-			EnemyRobotMovement erm = enemy.GetComponent<EnemyRobotMovement>();
-			EnemyRobotShoot ers = enemy.GetComponent<EnemyRobotShoot>();
-
-			erm.enabled = ers.enabled = true;
+			erf.Freeze(duration);
 		}
 	}
 }
