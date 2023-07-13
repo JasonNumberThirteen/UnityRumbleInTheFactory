6543135f7c4502e83f1a6a8a0efaba1aafcdb5d6
Jason <jasonxiii97@gmail.com>
2023-07-13 13:24:02 +0200

    Add a script for the robot animator

---
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/RobotAnimator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RobotAnimator : EntityAnimator
+{
+	protected override void SetValues()
+	{
+		Vector2 movementDirection = movement.Direction;
+
+		animator.SetFloat("MovementSpeed", MovementSpeed(movementDirection));
+
+		if(movementDirection != Vector2.zero)
+		{
+			animator.SetInteger("MovementX", (int)movementDirection.x);
+			animator.SetInteger("MovementY", (int)movementDirection.y);
+		}
+	}
+
+	private float MovementSpeed(Vector2 movementDirection) => (movementDirection.x != 0f || movementDirection.y != 0f) ? 1f : 0f;
+}
