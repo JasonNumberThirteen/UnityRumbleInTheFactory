ecef1c0bc796318b15c5ca43bcbcbbea3307c2f4
Jason <jasonxiii97@gmail.com>
2023-07-14 13:04:57 +0200

    Refactor code in the fortress bonus effect script

--- Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/FortressBonusEffect.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/FortressBonusEffect.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class FortressBonusEffect : TimedBonusEffect
 {
-	[Min(0f)] public float overlapBoxSize = 2.5f;
+	[Min(0f)] public float overlapBoxSize = 1.5f;
 	public LayerMask overlapLayers;
 	public GameObject metalTile;
 	
 	public override void PerformEffect()
 	{
 		GameObject nuke = GameObject.FindGameObjectWithTag("Nuke");
+		Collider2D nukeCollider = nuke.GetComponent<Collider2D>();
 		Collider2D[] colliders = Physics2D.OverlapBoxAll(nuke.transform.position, Vector2.one*overlapBoxSize, 0f, overlapLayers);
 
 		foreach (Collider2D collider in colliders)
@@ -16,16 +17,17 @@ public class FortressBonusEffect : TimedBonusEffect
 			Destroy(collider.gameObject);
 		}
 
-		for (int y = 0; y <= 3; ++y)
+		for (int y = 0; y <= 2; ++y)
 		{
-			for (int x = 0; x <= 5; ++x)
+			for (int x = 0; x <= 3; ++x)
 			{
-				if(x <= 1 || x >= 4 || y >= 2)
+				Vector2 topLeftPosition = new Vector2(-1.25f, -6.25f);
+				Vector2 offset = new Vector2(x, y)*0.5f;
+				Vector2 finalPosition = topLeftPosition + offset;
+				
+				if(!nukeCollider.OverlapPoint(finalPosition))
 				{
-					Vector2 topLeftPosition = new Vector2(-1.75f, -6.25f);
-					Vector2 offset = new Vector2(x, y)*0.5f;
-					
-					Instantiate(metalTile, topLeftPosition + offset, Quaternion.identity);
+					Instantiate(metalTile, finalPosition, Quaternion.identity);
 				}
 			}
 		}
