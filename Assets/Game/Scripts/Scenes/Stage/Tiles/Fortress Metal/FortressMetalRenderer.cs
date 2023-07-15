a48ff207829efe77203fa6cc519f2a6519a5d819
Jason <jasonxiii97@gmail.com>
2023-07-15 19:48:53 +0200

    Replace fortress metal tiles blink delay with remaining time threshold

--- Assets/Game/Scripts/Scenes/Stage/Tiles/Fortress Metal/FortressMetalRenderer.cs
+++ Assets/Game/Scripts/Scenes/Stage/Tiles/Fortress Metal/FortressMetalRenderer.cs
@@ -3,19 +3,25 @@ using UnityEngine;
 public class FortressMetalRenderer : MonoBehaviour
 {
 	public Sprite bricksTile;
-	[Min(0.01f)] public float blinkDelay = 5f;
+	[Min(0.01f)] public float timeForBlinkStart = 5f;
 	[Min(0.01f)] public float blinkDuration = 1f;
 	
 	private SpriteRenderer spriteRenderer;
 	private Sprite initialSprite;
 
 	public void SwapSprite() => spriteRenderer.sprite = (spriteRenderer.sprite == initialSprite) ? bricksTile : initialSprite;
-
+	
 	private void Awake()
 	{
 		spriteRenderer = GetComponent<SpriteRenderer>();
 		initialSprite = spriteRenderer.sprite;
 	}
 
-	private void Start() => InvokeRepeating("SwapSprite", blinkDelay, blinkDuration);
+	private void Start()
+	{
+		Timer timer = GetComponent<Timer>();
+		float blinkDelay = timer.duration - timeForBlinkStart;
+		
+		InvokeRepeating("SwapSprite", blinkDelay, blinkDuration);
+	}
 }
