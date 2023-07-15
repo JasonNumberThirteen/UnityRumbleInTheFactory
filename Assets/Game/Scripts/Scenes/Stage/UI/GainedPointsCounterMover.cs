5651c03eef357645673c01b237b8dc6ba82244db
Jason <jasonxiii97@gmail.com>
2023-07-15 18:29:48 +0200

    Add a script for the gained points counter mover

---
+++ Assets/Game/Scripts/Scenes/Stage/UI/GainedPointsCounterMover.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GainedPointsCounterMover : MonoBehaviour
+{
+	[Min(0.01f)] public float movementSpeed = 1f;
+	
+	private RectTransform rectTransform;
+	private RectTransformMover mover;
+	private Timer timer;
+
+	private void Awake()
+	{
+		rectTransform = GetComponent<RectTransform>();
+		mover = GetComponent<RectTransformMover>();
+		timer = GetComponent<Timer>();
+	}
+
+	private void Update() => mover.SetPositionY(rectTransform.anchoredPosition.y + movementSpeed*Time.deltaTime);
+}
