df20719410d6139d2410b85d003a17c316b60d76
Jason <jasonxiii97@gmail.com>
2023-07-15 21:43:01 +0200

    Snap bonus position to the grid when is spawned

--- Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/BonusPlacement.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/BonusPlacement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class BonusPlacement : MonoBehaviour
 {
 	public Vector2 bottomLeftArea, topRightArea;
+	[Min(0.01f)] public float gridSize = 0.5f;
 	
 	private void Start() => RandomisePosition();
 	private void RandomisePosition() => transform.position = RandomPosition();
@@ -12,7 +13,9 @@ public class BonusPlacement : MonoBehaviour
 	{
 		float x = Random.Range(bottomLeftArea.x, topRightArea.x);
 		float y = Random.Range(bottomLeftArea.y, topRightArea.y);
+		float gridX = Mathf.Round(x / gridSize)*gridSize;
+		float gridY = Mathf.Round(y / gridSize)*gridSize;
 
-		return new Vector2(x, y);
+		return new Vector2(gridX, gridY);
 	}
 }
