6fa459ce2bc8e586ad36abb432a94bfdbc81fdf0
Jason <jasonxiii97@gmail.com>
2023-07-20 19:17:49 +0200

    Recursively randomise bonus position to avoid being inaccessible

--- Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/BonusPlacement.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/BonusPlacement.cs
@@ -5,9 +5,10 @@ public class BonusPlacement : MonoBehaviour
 {
 	public Vector2 bottomLeftArea, topRightArea;
 	[Min(0.01f)] public float gridSize = 0.5f;
-	
-	private void Start() => RandomisePosition();
-	private void RandomisePosition() => transform.position = RandomPosition();
+	[Min(0.01f)] public float overlapCircleRadius = 0.5f;
+	public LayerMask excludedLayers;
+
+	private void Start() => transform.position = RandomPosition();
 
 	private Vector2 RandomPosition()
 	{
@@ -15,7 +16,46 @@ public class BonusPlacement : MonoBehaviour
 		float y = Random.Range(bottomLeftArea.y, topRightArea.y);
 		float gridX = Mathf.Round(x / gridSize)*gridSize;
 		float gridY = Mathf.Round(y / gridSize)*gridSize;
+		Vector2 position = new Vector2(gridX, gridY);
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(position, overlapCircleRadius, excludedLayers);
+		int excludedPositions = 0;
 
-		return new Vector2(gridX, gridY);
+		foreach (Collider2D collider in colliders)
+		{
+			if(collider.OverlapPoint(position))
+			{
+				++excludedPositions;
+
+				Debug.Log("Excluded positions: " + excludedPositions);
+			}
+		}
+
+		if(colliders.Length > 0 && excludedPositions == colliders.Length)
+		{
+			Debug.Log("Recursion at: (" + position.x + "; " + position.y + ")");
+
+			return RandomPosition();
+		}
+
+		return position;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, overlapCircleRadius, excludedLayers);
+		int excludedPositions = 0;
+		
+		foreach (Collider2D collider in colliders)
+		{
+			Gizmos.color = Color.green;
+
+			if(collider.OverlapPoint(transform.position))
+			{
+				Gizmos.color = Color.red;
+				++excludedPositions;
+			}
+			
+			Gizmos.DrawWireCube(collider.transform.position, collider.bounds.size);
+		}
 	}
 }
