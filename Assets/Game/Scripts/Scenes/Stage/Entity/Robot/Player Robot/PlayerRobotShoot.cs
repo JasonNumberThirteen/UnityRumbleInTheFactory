21c3aad257e1785878f1d0d438c363ca6b9c9283
Jason <jasonxiii97@gmail.com>
2023-07-23 15:34:13 +0200

    Add a script for the player robot shoot

---
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Player Robot/PlayerRobotShoot.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerRobotShoot : RobotShoot
+{
+	private PlayerRobotRank rank;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		rank = GetComponent<PlayerRobotRank>();
+	}
+	
+	public override void FireBullet()
+	{
+		if(GameObject.FindGameObjectsWithTag("Player Bullet").Length > rank.CurrentRank.bulletLimit)
+		{
+			return;
+		}
+		
+		base.FireBullet();
+	}
+}
