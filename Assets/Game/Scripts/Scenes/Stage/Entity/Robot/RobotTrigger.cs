30e059318380265f623ddcdb29c3876725269f3e
Jason <jasonxiii97@gmail.com>
2023-07-24 12:18:33 +0200

    Set damage receive from bullets dependant on their statistics

--- Assets/Game/Scripts/Scenes/Stage/Entity/Robot/RobotTrigger.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/RobotTrigger.cs
@@ -4,7 +4,15 @@ public class RobotTrigger : MonoBehaviour, ITriggerable
 {
 	protected RobotHealth health;
 	
-	public virtual void TriggerEffect(GameObject sender) => health.CurrentHealth -= 1;
+	public virtual void TriggerEffect(GameObject sender)
+	{
+		BulletStats bs = sender.GetComponent<BulletStats>();
+
+		if(bs != null)
+		{
+			health.CurrentHealth -= bs.damage;
+		}
+	}
 	
 	protected virtual void Awake() => health = GetComponent<RobotHealth>();
 }
