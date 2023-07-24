e3bf54fd2568d6af61b8db64244063a7fbd5124c
Jason <jasonxiii97@gmail.com>
2023-07-24 16:45:30 +0200

    Fix a bug with the destruction bonus effect which did not count defeated enemies

--- Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/Triggers/DestructionBonusTrigger.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/Triggers/DestructionBonusTrigger.cs
@@ -16,6 +16,9 @@ public class DestructionBonusTrigger : BonusTrigger
 			}
 		}
 
+		StageManager.instance.DefeatedEnemies += enemies.Length;
+
+		StageManager.instance.CheckEnemiesCount();
 		base.TriggerEffect(sender);
 	}
 }
