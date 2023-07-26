c7028c0964989ebc7da595f1327cad0e774a7c90
Jason <jasonxiii97@gmail.com>
2023-07-26 12:26:45 +0200

    Add a script for the score scene manager

---
+++ Assets/Game/Scripts/Scenes/Score/ScoreSceneManager.cs
@@ -0,0 +1,19 @@
+public class ScoreSceneManager : GameSceneManager
+{
+	public GameData gameData;
+	
+	public string stageSceneName, gameOverSceneName;
+
+	public void GoToNextScene()
+	{
+		bool isOver = gameData.isOver;
+		string sceneName = isOver ? gameOverSceneName : stageSceneName;
+		
+		if(!isOver)
+		{
+			++gameData.stage;
+		}
+
+		LoadScene(sceneName);
+	}
+}
