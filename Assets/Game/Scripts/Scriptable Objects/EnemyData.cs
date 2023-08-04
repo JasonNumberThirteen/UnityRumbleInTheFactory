030d6032b8b9c05abe647273439b7082d8cbe6f0
Jason <jasonxiii97@gmail.com>
2023-08-04 20:47:45 +0200

    Add a field for the prefab to the enemy data script

--- Assets/Game/Scripts/Scriptable Objects/EnemyData.cs
+++ Assets/Game/Scripts/Scriptable Objects/EnemyData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game/Enemy Data")]
 public class EnemyData : ScriptableObject
 {
+	public GameObject prefab;
 	[Min(0)] public int score;
 	public Sprite sprite;
 }
