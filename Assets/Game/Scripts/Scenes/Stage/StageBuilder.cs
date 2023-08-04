ed22a87ad6e9b8e069d64c6befad1d5d3e856258
Jason <jasonxiii97@gmail.com>
2023-08-04 21:49:33 +0200

    Refactor code in the stage builder script

--- Assets/Game/Scripts/Scenes/Stage/StageBuilder.cs
+++ Assets/Game/Scripts/Scenes/Stage/StageBuilder.cs
@@ -5,26 +5,33 @@ public class StageBuilder : MonoBehaviour
 	public GameData gameData;
 	public GameObject[] tiles;
 	public Vector2 positionOffset;
+	[Min(1)] public int stageWidthInTiles, stageHeightInTiles;
+	[Min(0.01f)] public float tileSize;
 
 	private void Start() => BuildStage();
+	private int BoardPositionX(int index) => index % stageWidthInTiles;
+	private int BoardPositionY(int index) => stageHeightInTiles - BoardPositionOffsetY(index);
+	private int BoardPositionOffsetY(int index) => Mathf.FloorToInt(index / stageHeightInTiles);
+	private Vector2 TilePosition(Vector2 position) => position*tileSize + positionOffset;
+	private bool TileExistsOnTheIndex(int index) => index >= 0 && index < tiles.Length;
 
 	private void BuildStage()
 	{
 		Stage stage = gameData.stages[gameData.stageNumber - 1];
+		int[] tilesIndexes = stage.tiles;
 
-		for (int i = 0; i < stage.tiles.Length; ++i)
+		for (int i = 0; i < tilesIndexes.Length; ++i)
 		{
-			int x = i % 26;
-			int y = 26 - Mathf.FloorToInt(i / 26);
-			int tileIndex = stage.tiles[i] - 1;
+			int tileIndex = tilesIndexes[i] - 1;
 
-			if(tileIndex >= 0 && tileIndex < tiles.Length)
+			if(TileExistsOnTheIndex(tileIndex))
 			{
-				float tileX = x*0.5f;
-				float tileY = y*0.5f;
-				Vector2 tilePosition = new Vector2(tileX, tileY);
+				int boardX = BoardPositionX(i);
+				int boardY = BoardPositionY(i);
+				Vector2 boardPosition = new Vector2(boardX, boardY);
+				Vector2 tilePosition = TilePosition(boardPosition);
 				
-				Instantiate(tiles[tileIndex], tilePosition + positionOffset, Quaternion.identity);
+				Instantiate(tiles[tileIndex], tilePosition, Quaternion.identity);
 			}
 		}
 	}
