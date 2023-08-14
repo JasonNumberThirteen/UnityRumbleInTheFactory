e09027fafbddbf0396f380491e7bbce5a683a87e
Jason <jasonxiii97@gmail.com>
2023-08-14 10:21:35 +0200

    Refactor code in the destruction bonus trigger script

--- Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/Triggers/DestructionBonusTrigger.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/Triggers/DestructionBonusTrigger.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class DestructionBonusTrigger : BonusTrigger
 {
+	public string enemyTag;
+	
 	public override void TriggerEffect(GameObject sender)
 	{
-		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
 
 		foreach (GameObject enemy in enemies)
 		{
-			EntityExploder ee = enemy.GetComponent<EntityExploder>();
-
-			if(ee != null)
+			if(enemy.TryGetComponent<EntityExploder>(out EntityExploder ee))
 			{
 				ee.Explode();
 			}
