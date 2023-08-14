d6943cba169406378ec183a30d390bd30ecf7842
Jason <jasonxiii97@gmail.com>
2023-08-14 11:12:20 +0200

    Refactor code in the shield bonus trigger script

--- Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/Triggers/ShieldBonusTrigger.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/Triggers/ShieldBonusTrigger.cs
@@ -4,9 +4,7 @@ public class ShieldBonusTrigger : TimedBonusTrigger
 {
 	public override void TriggerEffect(GameObject sender)
 	{
-		PlayerRobotShield prs = sender.GetComponent<PlayerRobotShield>();
-
-		if(prs != null)
+		if(sender.TryGetComponent(out PlayerRobotShield prs))
 		{
 			prs.ShieldTimer.duration = duration;
 
