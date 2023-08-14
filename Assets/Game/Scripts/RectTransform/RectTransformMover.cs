36e27b9c54e71c8b06a2a4b48daadc737cb1e451
Jason <jasonxiii97@gmail.com>
2023-08-14 16:18:35 +0200

    Add methods for setting and adding to RectTransform position as vectors

--- Assets/Game/Scripts/RectTransform/RectTransformMover.cs
+++ Assets/Game/Scripts/RectTransform/RectTransformMover.cs
@@ -6,6 +6,8 @@ public class RectTransformMover : MonoBehaviour
 
 	public void AddPositionX(float x) => SetPositionX(rectTransform.anchoredPosition.x + x);
 	public void AddPositionY(float y) => SetPositionY(rectTransform.anchoredPosition.y + y);
+	public void AddPosition(Vector2 position) => rectTransform.anchoredPosition += position;
+	public void SetPosition(Vector2 position) => rectTransform.anchoredPosition = position;
 	public void SetPositionX(float x) => rectTransform.anchoredPosition = new Vector2(x, rectTransform.anchoredPosition.y);
 	public void SetPositionY(float y) => rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, y);
 
