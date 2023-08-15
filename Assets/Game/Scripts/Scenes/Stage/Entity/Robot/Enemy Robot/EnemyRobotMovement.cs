c0535bf6573cc42761db83fae3954549d76784f9
Jason <jasonxiii97@gmail.com>
2023-08-15 15:42:28 +0200

    Refactor code in the enemy robot movement script

--- Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Enemy Robot/EnemyRobotMovement.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Enemy Robot/EnemyRobotMovement.cs
@@ -1,11 +1,7 @@
-using Random = UnityEngine.Random;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyRobotMovement : EntityMovement
 {
-	public LayerMask linecastDetectionLayers;
-	[Min(0.01f)] public float linecastDetectionDistance = 0.5f;
 	public Timer timer;
 	public RobotCollisionDetector collisionDetector;
 
@@ -13,6 +9,7 @@ public class EnemyRobotMovement : EntityMovement
 	private EnemyRobotFreeze freeze;
 	private float lastMovementSpeed;
 	private Vector2 lastDirection;
+	private EnemyRobotMovementDirectionSelector movementDirectionSelector;
 
 	public void SetMovementLock()
 	{
@@ -24,7 +21,7 @@ public class EnemyRobotMovement : EntityMovement
 
 	public void RandomiseDirection()
 	{
-		Vector2 direction = RandomDirection();
+		Vector2 direction = movementDirectionSelector.RandomDirection(Direction);
 
 		SetDirections(direction, direction);
 	}
@@ -40,6 +37,7 @@ public class EnemyRobotMovement : EntityMovement
 		base.Awake();
 
 		freeze = GetComponent<EnemyRobotFreeze>();
+		movementDirectionSelector = GetComponent<EnemyRobotMovementDirectionSelector>();
 	}
 
 	protected override void FixedUpdate()
@@ -69,38 +67,7 @@ public class EnemyRobotMovement : EntityMovement
 
 	private void Start() => SetDirection(Vector2.down);
 	private bool LastDirectionIsNotZero() => lastDirection != Vector2.zero;
-
-	private Vector2 RandomDirection()
-	{
-		List<Vector2> directions = AvailableDirections();
-		Vector2 randomDirection = RandomAvailableDirection(directions);
-
-		if(directions.Count > 1 && randomDirection == Direction)
-		{
-			return RandomDirection();
-		}
-
-		return randomDirection;
-	}
-
-	private List<Vector2> AvailableDirections()
-	{
-		List<Vector2> directions = new List<Vector2>{Vector2.up, Vector2.down, Vector2.left, Vector2.right};
-		Vector2 start = transform.position;
-
-		directions.RemoveAll(e => Physics2D.Linecast(start, start + e*linecastDetectionDistance, linecastDetectionLayers));
-
-		return directions;
-	}
-
-	private Vector2 RandomAvailableDirection(List<Vector2> directions)
-	{
-		int index = RandomAvailableDirectionIndex(directions);
-
-		return directions[index];
-	}
-
-	private int RandomAvailableDirectionIndex(List<Vector2> directions) => Random.Range(0, directions.Count);
+	private bool DetectedCollision() => !detectedCollision && !freeze.Frozen && collisionDetector.OverlapBoxAll().Length > 1;
 	
 	private void DetectObstacles()
 	{
@@ -109,8 +76,6 @@ public class EnemyRobotMovement : EntityMovement
 			DisableCollisionDetection();
 		}
 	}
-
-	private bool DetectedCollision() => !detectedCollision && !freeze.Frozen && collisionDetector.OverlapBoxAll().Length > 1;
 
 	private void DisableCollisionDetection()
 	{
@@ -122,6 +87,12 @@ public class EnemyRobotMovement : EntityMovement
 	}
 
 	private void OnDrawGizmos()
+	{
+		DrawDetectedColliders();
+		DrawAvailableDirections();
+	}
+
+	private void DrawDetectedColliders()
 	{
 		if(collisionDetector == null)
 		{
@@ -136,22 +107,30 @@ public class EnemyRobotMovement : EntityMovement
 			
 			Gizmos.DrawWireCube(collider.transform.position, collider.bounds.size);
 		}
+	}
+
+	private void DrawAvailableDirections()
+	{
+		if(movementDirectionSelector == null)
+		{
+			return;
+		}
 
 		Vector2[] directions = {Vector2.up, Vector2.down, Vector2.left, Vector2.right};
 
 		foreach (Vector2 direction in directions)
 		{
 			Vector2 start = transform.position;
-			Vector2 end = start + direction*linecastDetectionDistance;
+			Vector2 end = start + direction*movementDirectionSelector.obstacleDetectionDistance;
 
 			Gizmos.color = Color.white;
 
-			if(Physics2D.Linecast(start, end, linecastDetectionLayers))
+			if(Physics2D.Linecast(start, end, movementDirectionSelector.obstacleDetectionLayers))
 			{
 				Gizmos.color = Color.red;
 			}
 			
-			Gizmos.DrawLine(start, start + direction*linecastDetectionDistance);
+			Gizmos.DrawLine(start, end);
 		}
 	}
 }
