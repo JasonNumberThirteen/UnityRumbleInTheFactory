42c7b4594d26f3b8a4c54cf0b0588c1b30b47222
Jason <jasonxiii97@gmail.com>
2023-08-15 17:54:44 +0200

    Add a method reducing robot health points

--- Assets/Game/Scripts/Scenes/Stage/Entity/Robot/RobotHealth.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/RobotHealth.cs
@@ -4,28 +4,22 @@ public class RobotHealth : MonoBehaviour, IUpgradeable
 {
 	[Min(1)] public int initialHealth = 1;
 
-	public int CurrentHealth
-	{
-		get
-		{
-			return health;
-		}
-		set
-		{
-			health = value;
+	public int Health {get; private set;}
 
-			CheckHealth();
-		}
+	public virtual void UpdateValues(Rank rank) => Health = rank.health;
+
+	public virtual void TakeDamage(int damage)
+	{
+		Health -= damage;
+
+		CheckHealth();
 	}
 
-	private int health;
-
-	public virtual void UpdateValues(Rank rank) => CurrentHealth = rank.health;
-	protected virtual void Start() => CurrentHealth = initialHealth;
+	protected virtual void Start() => Health = initialHealth;
 
 	protected virtual void CheckHealth()
 	{
-		if(CurrentHealth <= 0)
+		if(Health <= 0)
 		{
 			Die();
 		}
