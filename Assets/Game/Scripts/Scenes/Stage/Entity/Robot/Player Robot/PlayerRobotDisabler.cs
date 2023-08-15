5de1210f1f61e3696a6d669e1210245607bed9bf
Jason <jasonxiii97@gmail.com>
2023-08-15 18:36:56 +0200

    Add a script for the player robot disabler

---
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Player Robot/PlayerRobotDisabler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerRobotDisabler : MonoBehaviour
+{
+	public void DisableYourself()
+	{
+		if(TryGetComponent(out EntityMovement em))
+		{
+			em.Direction = Vector2.zero;
+
+			Destroy(em);
+		}
+
+		if(TryGetComponent(out RobotShoot rs))
+		{
+			Destroy(rs);
+		}
+	}
+}
