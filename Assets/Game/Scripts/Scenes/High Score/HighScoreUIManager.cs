e2c88dd3eaf5a4203db852d39270873fe2dc45c3
Jason <jasonxiii97@gmail.com>
2023-08-17 11:53:47 +0200

    Refactor code in the high score UI manager script

--- Assets/Game/Scripts/Scenes/High Score/HighScoreUIManager.cs
+++ Assets/Game/Scripts/Scenes/High Score/HighScoreUIManager.cs
@@ -1,11 +1,9 @@
-using TMPro;
 using UnityEngine;
 
 public class HighScoreUIManager : MonoBehaviour
 {
 	public GameData gameData;
-	public TextMeshProUGUI highScoreText;
+	public Counter counter;
 
-	private void Start() => highScoreText.text = "HISCORE\n" + FormattedScore(gameData.highScore);
-	private string FormattedScore(int score) => string.Format("{0,7}", score);
+	private void Awake() => counter.SetTo(gameData.highScore);
 }
