6885d2c45cf5e0b3afd933be86401b8b0a542086
Jason <jasonxiii97@gmail.com>
2023-10-04 13:36:04 +0200

    Add a script for the looping counter

---
+++ Assets/Game/Scripts/Counter/LoopingCounter.cs
@@ -0,0 +1,21 @@
+public class LoopingCounter : Counter
+{
+	public int min, max;
+
+	protected override void IncreaseValue(int value)
+	{
+		int nextValue = NextValue(value);
+
+		SetTo(nextValue);
+	}
+
+	protected override void DecreaseValue(int value)
+	{
+		int previousValue = PreviousValue(value);
+
+		SetTo(previousValue);
+	}
+
+	private int NextValue(int value) => (CurrentValue + value - 1) % max + min;
+	private int PreviousValue(int value) => (CurrentValue - value + max - 1) % max + min;
+}
