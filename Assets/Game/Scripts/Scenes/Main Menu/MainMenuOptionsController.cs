0128e787661bcd876bda4d3b3f192773b06f9792
Jason <jasonxiii97@gmail.com>
2023-10-10 12:19:49 +0200

    Refactor code in the main menu options controller script

--- Assets/Game/Scripts/Scenes/Main Menu/MainMenuOptionsController.cs
+++ Assets/Game/Scripts/Scenes/Main Menu/MainMenuOptionsController.cs
@@ -3,37 +3,20 @@ using UnityEngine;
 public class MainMenuOptionsController : MonoBehaviour
 {
 	public Option[] options;
+	public LoopingCounter counter;
 
-	private int optionIndex;
+	public void SelectOption() => CurrentOption().onSelect.Invoke();
+	public void SubmitOption() => CurrentOption().onSubmit.Invoke();
+	public void IncreaseOptionValue() => counter.IncreaseBy(1);
+	public void DecreaseOptionValue() => counter.DecreaseBy(1);
 
-	public void SelectOption() => options[optionIndex].onSelect.Invoke();
-	public void SubmitOption() => options[optionIndex].onSubmit.Invoke();
+	private void Awake() => SetCounterRange();
+	private Option CurrentOption() => options[CounterValueIndex()];
+	private int CounterValueIndex() => counter.CurrentValue - 1;
 
-	public void IncreaseOptionValue()
+	private void SetCounterRange()
 	{
-		++optionIndex;
-
-		ClampOptionValue();
-	}
-
-	public void DecreaseOptionValue()
-	{
-		--optionIndex;
-
-		ClampOptionValue();
-	}
-
-	private void ClampOptionValue()
-	{
-		int lastOptionIndex = options.Length - 1;
-
-		if(optionIndex < 0)
-		{
-			optionIndex = lastOptionIndex;
-		}
-		else if(optionIndex > lastOptionIndex)
-		{
-			optionIndex = 0;
-		}
+		counter.min = 1;
+		counter.max = options.Length;
 	}
 }
