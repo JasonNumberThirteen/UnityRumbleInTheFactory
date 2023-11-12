560308fe644b51b36fc47b3503889f6f7aec19d6
Jason <jasonxiii97@gmail.com>
2023-11-12 21:27:27 +0100

    Add a script for the left enemy icons manager

---
+++ Assets/Game/Scripts/Scenes/Stage/Managers/LeftEnemyIconsManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class LeftEnemyIconsManager : MonoBehaviour
+{
+	public RectTransform hud;
+	public GameObject leftEnemyIcon;
+	[Min(0)] public int leftEnemyIconsLimit = 20;
+	
+	private GameObject[] leftEnemyIcons;
+	private int leftEnemyIconIndex;
+
+	public void DestroyLeftEnemyIcon()
+	{
+		if(--leftEnemyIconIndex < leftEnemyIcons.Length)
+		{
+			Destroy(leftEnemyIcons[leftEnemyIconIndex]);
+		}
+	}
+	
+	private void Start() => InstantiateIcons();
+
+	private int LeftEnemyIconsCount(int enemiesCount) => Mathf.Min(enemiesCount, leftEnemyIconsLimit);
+	private int LeftEnemyIconX(int index) => 8*(index % 2);
+	private int LeftEnemyIconY(int index) => -8*(index >> 1);
+	private Vector2 LeftEnemyIconPosition(int index)
+	{
+		int offsetX = LeftEnemyIconX(index);
+		int offsetY = LeftEnemyIconY(index);
+		Vector2 initialPosition = new Vector2(-16, -16);
+		Vector2 offset = new Vector2(offsetX, offsetY);
+		
+		return initialPosition + offset;
+	}
+
+	private void InstantiateIcons()
+	{
+		int enemiesCount = StageManager.instance.enemySpawnManager.EnemiesCount();
+		int iconsCount = LeftEnemyIconsCount(enemiesCount);
+
+		leftEnemyIcons = new GameObject[iconsCount];
+		leftEnemyIconIndex = enemiesCount;
+
+		for (int i = 0; i < iconsCount; ++i)
+		{
+			InstantiateLeftEnemyIcon(i);
+		}
+	}
+
+	private void InstantiateLeftEnemyIcon(int index)
+	{
+		GameObject instance = Instantiate(leftEnemyIcon, hud.transform);
+		
+		if(instance.TryGetComponent(out RectTransformMover rtm))
+		{
+			rtm.SetPosition(LeftEnemyIconPosition(index));
+		}
+
+		leftEnemyIcons[index] = instance;
+	}
+}
