020fb535455d2b5773ca7d3383f4985e48c9e585
Jason <jasonxiii97@gmail.com>
2023-11-12 21:10:56 +0100

    Refactor code in the gained points counter mover script

--- Assets/Game/Scripts/Scenes/Stage/UI/Gained Points Counter/GainedPointsCounterMover.cs
+++ Assets/Game/Scripts/Scenes/Stage/UI/Gained Points Counter/GainedPointsCounterMover.cs
@@ -4,16 +4,8 @@ public class GainedPointsCounterMover : MonoBehaviour
 {
 	[Min(0.01f)] public float movementSpeed = 1f;
 	
-	private RectTransform rectTransform;
 	private RectTransformMover mover;
-	private Timer timer;
 
-	private void Awake()
-	{
-		rectTransform = GetComponent<RectTransform>();
-		mover = GetComponent<RectTransformMover>();
-		timer = GetComponent<Timer>();
-	}
-
+	private void Awake() => mover = GetComponent<RectTransformMover>();
 	private void Update() => mover.AddPositionY(movementSpeed*Time.deltaTime);
 }
