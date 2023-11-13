2bb2b7fe602bc7157e3e5f5e22748f30855d2dd6
Jason <jasonxiii97@gmail.com>
2023-11-13 14:18:09 +0100

    Remove the bonus type prefab field from the enemy data script

--- Assets/Game/Scripts/Scriptable Objects/EnemyData.cs
+++ Assets/Game/Scripts/Scriptable Objects/EnemyData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game/Enemy Data")]
 public class EnemyData : ScriptableObject
 {
-	public GameObject prefab, bonusTypePrefab;
+	public GameObject prefab;
 	[Min(0)] public int score;
 	public Sprite sprite;
 }
