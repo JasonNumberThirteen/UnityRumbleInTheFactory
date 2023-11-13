38919894911fd032ad2e4e887e116874843b3dfa
Jason <jasonxiii97@gmail.com>
2023-11-13 16:28:25 +0100

    Fix getting reference to the player's shield

--- Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Player Robot/PlayerRobotShield.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Player Robot/PlayerRobotShield.cs
@@ -6,5 +6,5 @@ public class PlayerRobotShield : MonoBehaviour
 	
 	public Timer ShieldTimer {get; private set;}
 
-	private void Awake() => ShieldTimer = GameObject.FindGameObjectWithTag(shieldTag).GetComponent<Timer>();
+	private void Awake() => ShieldTimer = GetComponentInChildren<Timer>();
 }
