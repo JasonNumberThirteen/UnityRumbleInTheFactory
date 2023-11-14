5115889ecb49103a59f7802e7e86cd70a8e8ec13
Jason <jasonxiii97@gmail.com>
2023-11-14 16:45:01 +0100

    Add a script for the stage state manager

---
+++ Assets/Game/Scripts/Scenes/Stage/Managers/StageStateManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class StageStateManager : MonoBehaviour
+{
+	private GameStates state = GameStates.ACTIVE;
+
+	private enum GameStates
+	{
+		ACTIVE, PAUSED, INTERRUPTED, WON, OVER
+	}
+
+	public bool GameIsOver() => IsInterrupted() || IsOver();
+	public bool IsActive() => state == GameStates.ACTIVE;
+	public bool IsPaused() => state == GameStates.PAUSED;
+	public bool IsInterrupted() => state == GameStates.INTERRUPTED;
+	public bool IsWon() => state == GameStates.WON;
+	public bool IsOver() => state == GameStates.OVER;
+	public void SetAsActive() => state = GameStates.ACTIVE;
+	public void SetAsPaused() => state = GameStates.PAUSED;
+	public void SetAsInterrupted() => state = GameStates.INTERRUPTED;
+	public void SetAsWon() => state = GameStates.WON;
+	public void SetAsOver() => state = GameStates.OVER;
+}
