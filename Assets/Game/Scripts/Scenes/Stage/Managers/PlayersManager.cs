c3287b768b46fd9131f9fb5a34386f995b1f2546
Jason <jasonxiii97@gmail.com>
2023-11-14 21:54:23 +0100

    Add a script for the players manager

---
+++ Assets/Game/Scripts/Scenes/Stage/Managers/PlayersManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour
+{
+	public string playerTag, playerSpawnerTag;
+	public Timer gameOverTimer;
+	
+	private PlayerData[] playersData;
+
+	public void ResetDefeatedEnemiesByPlayer()
+	{
+		foreach (PlayerData pd in playersData)
+		{
+			pd.ResetDefeatedEnemies();
+		}
+	}
+
+	public void CheckPlayersLives()
+	{
+		if(AllPlayersLostAllLives())
+		{
+			gameOverTimer.onEnd.Invoke();
+		}
+	}
+
+	public void DisablePlayers()
+	{
+		GameObject[] players = FoundObjectsWithTag(playerTag);
+
+		foreach (GameObject player in players)
+		{
+			if(player.TryGetComponent(out PlayerRobotDisabler prd))
+			{
+				prd.DisableYourself();
+			}
+		}
+	}
+
+	public void FindPlayers()
+	{
+		GameObject[] spawners = FoundObjectsWithTag(playerSpawnerTag);
+		int length = spawners.Length;
+
+		playersData = new PlayerData[length];
+
+		for (int i = 0; i < length; ++i)
+		{
+			if(spawners[i].TryGetComponent(out PlayerSpawner ps))
+			{
+				playersData[i] = ps.playerData;
+			}
+		}
+	}
+
+	private GameObject[] FoundObjectsWithTag(string tag) => GameObject.FindGameObjectsWithTag(tag);
+
+	private bool AllPlayersLostAllLives()
+	{
+		foreach (PlayerData pd in playersData)
+		{
+			if(!pd.lostAllLives)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
