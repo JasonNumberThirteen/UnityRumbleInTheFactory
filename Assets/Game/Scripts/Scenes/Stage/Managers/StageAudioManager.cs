c3931c0263e95e4504441ff869986b3625ab81f9
Jason <jasonxiii97@gmail.com>
2023-11-28 14:27:08 +0100

    Add several audio sources to the stage audio manager

--- Assets/Game/Scripts/Scenes/Stage/Managers/StageAudioManager.cs
+++ Assets/Game/Scripts/Scenes/Stage/Managers/StageAudioManager.cs
@@ -4,19 +4,34 @@ public class StageAudioManager : MonoBehaviour
 {
 	public AudioClip playerRobotBulletHit, enemyRobotExplosion, bonusCollect;
 
-	private AudioSource audioSource;
+	private AudioSource[] audioSources;
 
 	public void PlayPlayerRobotBulletHitSound() => PlaySound(playerRobotBulletHit);
 	public void PlayEnemyRobotExplosionSound() => PlaySound(enemyRobotExplosion);
 	public void PlayBonusCollectSound() => PlaySound(bonusCollect);
 
-	private void Awake() => audioSource = GetComponent<AudioSource>();
+	private void Awake() => audioSources = GetComponentsInChildren<AudioSource>();
 
 	private void PlaySound(AudioClip audioClip)
 	{
 		if(audioClip != null)
 		{
-			audioSource.PlayOneShot(audioClip);
+			AudioSource freeAudioSource = null;
+
+			for (int i = 0; i < audioSources.Length && freeAudioSource == null; ++i)
+			{
+				AudioSource current = audioSources[i];
+
+				if(!current.isPlaying)
+				{
+					freeAudioSource = current;
+				}
+			}
+			
+			if(freeAudioSource != null)
+			{
+				freeAudioSource.PlayOneShot(audioClip);
+			}
 		}
 	}
 }
