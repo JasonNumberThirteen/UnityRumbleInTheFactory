53c981ec18e21a51c6d62bb3222c9f40d96402be
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-24 13:32:25 +0100

    Remove shortcut from single statement methods in the text UI blinker script

--- Assets/Game/Scripts/Scenes/Stage/UI/TextUIBlinker.cs
+++ Assets/Game/Scripts/Scenes/Stage/UI/TextUIBlinker.cs
@@ -8,7 +8,15 @@ public class TextUIBlinker : MonoBehaviour
 	
 	private TextMeshProUGUI text;
 
-	private void Awake() => text = GetComponent<TextMeshProUGUI>();
-	private void Update() => text.enabled = ReachedBlinkDelay();
+	private void Awake()
+	{
+		text = GetComponent<TextMeshProUGUI>();
+	}
+
+	private void Update()
+	{
+		text.enabled = ReachedBlinkDelay();
+	}
+	
 	private bool ReachedBlinkDelay() => Time.unscaledTime % (blinkDelay*2) >= blinkDelay;
 }
