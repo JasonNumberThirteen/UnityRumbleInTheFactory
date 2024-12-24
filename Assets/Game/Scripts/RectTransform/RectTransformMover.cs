6ca3a1fc63746df4879fce341aaf6b3c0eec0bf7
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-24 12:11:14 +0100

    Remove shortcut from single statement methods in the RectTransform mover script

--- Assets/Game/Scripts/RectTransform/RectTransformMover.cs
+++ Assets/Game/Scripts/RectTransform/RectTransformMover.cs
@@ -8,12 +8,39 @@ public class RectTransformMover : MonoBehaviour
 	public float GetPositionX() => GetPosition().x;
 	public float GetPositionY() => GetPosition().y;
 	public Vector2 GetPosition() => rectTransform.anchoredPosition;
-	public void AddPositionX(float x) => SetPositionX(rectTransform.anchoredPosition.x + x);
-	public void AddPositionY(float y) => SetPositionY(rectTransform.anchoredPosition.y + y);
-	public void AddPosition(Vector2 position) => rectTransform.anchoredPosition += position;
-	public void SetPosition(Vector2 position) => rectTransform.anchoredPosition = position;
-	public void SetPositionX(float x) => rectTransform.anchoredPosition = new Vector2(x, rectTransform.anchoredPosition.y);
-	public void SetPositionY(float y) => rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, y);
 
-	protected virtual void Awake() => rectTransform = GetComponent<RectTransform>();
+	public void AddPositionX(float x)
+	{
+		SetPositionX(rectTransform.anchoredPosition.x + x);
+	}
+
+	public void AddPositionY(float y)
+	{
+		SetPositionY(rectTransform.anchoredPosition.y + y);
+	}
+
+	public void AddPosition(Vector2 position)
+	{
+		rectTransform.anchoredPosition += position;
+	}
+
+	public void SetPosition(Vector2 position)
+	{
+		rectTransform.anchoredPosition = position;
+	}
+
+	public void SetPositionX(float x)
+	{
+		rectTransform.anchoredPosition = new Vector2(x, rectTransform.anchoredPosition.y);
+	}
+
+	public void SetPositionY(float y)
+	{
+		rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, y);
+	}
+
+	protected virtual void Awake()
+	{
+		rectTransform = GetComponent<RectTransform>();
+	}
 }
