c791e910fef316fc939272efa3b843b16e081bcf
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-24 12:50:04 +0100

    Add a null check in the one player mode excluder script

--- Assets/Game/Scripts/OnePlayerModeExcluder.cs
+++ Assets/Game/Scripts/OnePlayerModeExcluder.cs
@@ -4,5 +4,10 @@ public class OnePlayerModeExcluder : MonoBehaviour
 {
 	[SerializeField] private GameData gameData;
 
-	private void Awake() => gameObject.SetActive(gameData.twoPlayersMode);
+	private void Awake()
+	{
+		var goShouldBeActive = gameData != null && gameData.twoPlayersMode;
+		
+		gameObject.SetActive(goShouldBeActive);
+	}
 }
