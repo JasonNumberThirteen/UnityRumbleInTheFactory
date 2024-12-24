c1d22b909b4415ea90d1243849894a923525f19f
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-24 14:03:33 +0100

    Add an event when stage state was changed in the stage state manager script

--- Assets/Game/Scripts/Scenes/Stage/Managers/StageStateManager.cs
+++ Assets/Game/Scripts/Scenes/Stage/Managers/StageStateManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class StageStateManager : MonoBehaviour
 {
+	public UnityEvent<StageState> stageStateChangedEvent;
+	
 	private StageState stageState = StageState.ACTIVE;
 
 	public bool StateIsSetTo(StageState stageState) => this.stageState == stageState;
@@ -10,5 +13,7 @@ public class StageStateManager : MonoBehaviour
 	public void SetStateTo(StageState stageState)
 	{
 		this.stageState = stageState;
+
+		stageStateChangedEvent?.Invoke(stageState);
 	}
 }
