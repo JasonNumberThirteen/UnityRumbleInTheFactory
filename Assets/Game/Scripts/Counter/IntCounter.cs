b3c58385f5a886c460bdd17e7aa2af7dcbb2a2d3
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-25 20:53:19 +0100

    Add an event when value was changed in the integer counter script

--- Assets/Game/Scripts/Counter/IntCounter.cs
+++ Assets/Game/Scripts/Counter/IntCounter.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class IntCounter : MonoBehaviour
 {
 	public int initialValue;
 	public CounterText text;
 	public bool setAtStart = true;
+	public UnityEvent valueChangedEvent;
 
 	public int CurrentValue {get; private set;}
 
-	protected virtual void SetValue(int value) => CurrentValue = value;
-	protected virtual void IncreaseValue(int value) => CurrentValue += value;
-	protected virtual void DecreaseValue(int value) => CurrentValue -= value;
+	protected virtual void SetValue(int value)
+	{
+		CurrentValue = value;
+
+		valueChangedEvent?.Invoke();
+	}
+
+	protected virtual void IncreaseValue(int value)
+	{
+		CurrentValue += value;
+
+		valueChangedEvent?.Invoke();
+	}
+
+	protected virtual void DecreaseValue(int value)
+	{
+		CurrentValue -= value;
+
+		valueChangedEvent?.Invoke();
+	}
 
 	public void SetTo(int value)
 	{
