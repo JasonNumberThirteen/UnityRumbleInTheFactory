c3062778d85406ee7fec86480234aefc359a9606
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-26 18:48:45 +0100

    Refactor timer in the game over scene

--- Assets/Game/Scripts/Scenes/Game Over/GameOverGameSceneManager.cs
+++ Assets/Game/Scripts/Scenes/Game Over/GameOverGameSceneManager.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Timer))]
 public class GameOverGameSceneManager : GameSceneManager
 {
 	[SerializeField] private GameData gameData;
 
-	public void GoToNextScene()
+	private Timer timer;
+
+	private void Awake()
 	{
-		var beatenHighScore = gameData.beatenHighScore;
+		timer = GetComponent<Timer>();
+
+		RegisterToListeners(true);
+	}
+
+	private void OnDestroy()
+	{
+		RegisterToListeners(false);
+	}
+	
+	private void RegisterToListeners(bool register)
+	{
+		if(register)
+		{
+			timer.onEnd.AddListener(OnTimerEnd);
+		}
+		else
+		{
+			timer.onEnd.RemoveListener(OnTimerEnd);
+		}
+	}
+
+	private void OnTimerEnd()
+	{
+		var beatenHighScore = gameData != null && gameData.beatenHighScore;
 		var sceneName = beatenHighScore ? HIGH_SCORE_SCENE_NAME : MAIN_MENU_SCENE_NAME;
 
 		LoadSceneByName(sceneName);
