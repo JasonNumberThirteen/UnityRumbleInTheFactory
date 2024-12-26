ad8addee390dccc6a56df357455073651e261b80
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-26 18:27:32 +0100

    Fix exception while setting position to the main menu options cursor on start

--- Assets/Game/Scripts/Scenes/Main Menu/MainMenuOptionsCursor.cs
+++ Assets/Game/Scripts/Scenes/Main Menu/MainMenuOptionsCursor.cs
@@ -1,22 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(RectTransformPositionController))]
+[RequireComponent(typeof(Image), typeof(RectTransformPositionController))]
 public class MainMenuOptionsCursor : MonoBehaviour
 {
+	private Image image;
 	private RectTransformPositionController rectTransformPositionController;
-	
+
 	public void SetActive(bool active)
 	{
-		gameObject.SetActive(active);
+		image.enabled = active;
 	}
 
 	public void SetPositionY(float y)
 	{
-		rectTransformPositionController.SetPositionY(y);	
+		rectTransformPositionController.SetPositionY(y);
 	}
 
 	private void Awake()
 	{
+		image = GetComponent<Image>();
 		rectTransformPositionController = GetComponent<RectTransformPositionController>();
+	}
+
+	private void Start()
+	{
+		SetActive(false);
 	}
 }
