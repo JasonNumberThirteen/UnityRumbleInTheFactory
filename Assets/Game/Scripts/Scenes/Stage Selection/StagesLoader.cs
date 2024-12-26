205d909f830f4de0fa607ce4276cb0c01a88fdf9
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-26 19:49:20 +0100

    Refactor code in the stages loader script

--- Assets/Game/Scripts/Scenes/Stage Selection/StagesLoader.cs
+++ Assets/Game/Scripts/Scenes/Stage Selection/StagesLoader.cs
@@ -2,26 +2,48 @@ using UnityEngine;
 
 public class StagesLoader : MonoBehaviour
 {
-	public LoopingCounter stageCounter;
-	public GameData gameData;
-	public string directory;
+	[SerializeField] private LoopingCounter loopingCounter;
+	[SerializeField] private GameData gameData;
 
-	private void Awake() => gameData.stages = DetectedStages();
-	private void Start() => stageCounter.max = gameData.stages.Length;
+	private readonly string STAGES_DATA_PATH = "Stages";
 
-	private StageData[] DetectedStages()
+	private void Awake()
 	{
-		Object[] data = Resources.LoadAll(directory, typeof(TextAsset));
-		int dataLength = data.Length;
-		StageData[] stages = new StageData[dataLength];
-
-		for (int i = 0; i < dataLength; ++i)
+		if(gameData != null)
 		{
-			TextAsset ta = (TextAsset)data[i];
-			
-			stages[i] = JsonUtility.FromJson<StageData>(ta.text);
+			gameData.stages = GetStagesDataFromPath(STAGES_DATA_PATH);
+		}
+	}
+
+	private void Start()
+	{
+		if(loopingCounter != null && gameData != null && gameData.stages != null && gameData.stages.Length > 0)
+		{
+			loopingCounter.SetRange(1, gameData.stages.Length);
+		}
+	}
+
+	private StageData[] GetStagesDataFromPath(string path)
+	{
+		var assets = Resources.LoadAll(path, typeof(TextAsset));
+		var numberOfAssets = assets.Length;
+		var stagesData = new StageData[numberOfAssets];
+
+		for (var i = 0; i < numberOfAssets; ++i)
+		{
+			stagesData[i] = GetStageDataFromAsset(assets[i]);
 		}
 
-		return stages;
+		return stagesData;
+	}
+
+	private StageData GetStageDataFromAsset(Object asset)
+	{
+		if(asset is TextAsset textAsset)
+		{
+			return JsonUtility.FromJson<StageData>(textAsset.text);
+		}
+
+		return null;
 	}
 }
