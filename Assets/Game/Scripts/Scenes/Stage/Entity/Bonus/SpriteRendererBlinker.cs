0a7e87a90a399ec15abb7a40a2743686fb67ba36
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-27 12:20:43 +0100

    Refactor code in the sprite renderer blinker

--- Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/SpriteRendererBlinker.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Bonus/SpriteRendererBlinker.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 
 public class SpriteRendererBlinker : MonoBehaviour
 {
-	[Min(0.01f)] public float blinkDelay = 1f;
+	[SerializeField, Min(0.01f)] private float blinkDelay = 1f;
 	
 	private SpriteRenderer spriteRenderer;
 
-	private void Awake() => spriteRenderer = GetComponent<SpriteRenderer>();
-	private void Start() => InvokeRepeating(nameof(SwitchRendererEnable), blinkDelay, blinkDelay);
-	private void SwitchRendererEnable() => spriteRenderer.enabled = !spriteRenderer.enabled;
+	private void Awake()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	private void Start()
+	{
+		InvokeRepeating(nameof(SwitchEnable), blinkDelay, blinkDelay);
+	}
+
+	private void SwitchEnable()
+	{
+		spriteRenderer.enabled = !spriteRenderer.enabled;
+	}
 }
