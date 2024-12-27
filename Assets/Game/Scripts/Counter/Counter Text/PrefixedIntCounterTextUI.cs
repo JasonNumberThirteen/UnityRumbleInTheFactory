779232eca749bc8ea2f3f46db0fb94eac36890f6
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-27 15:02:12 +0100

    Add a shortcut to the single expression method in the prefixed integer counter text UI script

--- Assets/Game/Scripts/Counter/Counter Text/PrefixedIntCounterTextUI.cs
+++ Assets/Game/Scripts/Counter/Counter Text/PrefixedIntCounterTextUI.cs
@@ -4,10 +4,7 @@ public class PrefixedIntCounterTextUI : IntCounterTextUI
 {
 	[SerializeField] private int width;
 
-	public override string GetCounterValueAsString()
-	{
-		return string.Format("{0," + width + "}", GetFormattedCounterValueAsString());
-	}
+	public override string GetCounterValueAsString() => string.Format("{0," + width + "}", GetFormattedCounterValueAsString());
 
 	public virtual string GetFormattedCounterValueAsString() => base.GetCounterValueAsString();
 }
