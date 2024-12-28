16699dbf77405c679a4496c39fef475b0b2ffb56
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-28 11:33:33 +0100

    Replace inconsistent arrays with game difficulty tiers in the game difficulty script

--- Assets/Game/Scripts/GameDifficulty.cs
+++ Assets/Game/Scripts/GameDifficulty.cs
@@ -5,15 +5,12 @@ using UnityEngine;
 public class GameDifficulty
 {
 	[SerializeField] [Min(0)] private int tier;
-	[SerializeField] [Min(0)] private int maxTier = 5;
-
-	[SerializeField] private float[] enemiesMovementSpeedMultiplierPerTier;
-	[SerializeField] private int[] enemiesLimitPerTier;
+	[SerializeField] private GameDifficultyTier[] tiers;
 
 	public void ResetData() => tier = 0;
-	public float EnemiesMovementSpeedMultiplier() => enemiesMovementSpeedMultiplierPerTier[tier];
-	public int EnemiesLimit() => enemiesLimitPerTier[tier];
+	public float EnemiesMovementSpeedMultiplier() => tiers[tier].GetEnemyMovementSpeedMultiplier();
+	public int EnemiesLimit() => tiers[tier].GetEnemiesLimitAtOnce();
 	public int CurrentTier() => tier;
 
-	public void IncreaseDifficulty() => tier = Mathf.Clamp(tier + 1, 0, maxTier);
+	public void IncreaseDifficulty() => tier = Mathf.Clamp(tier + 1, 0, tiers.Length - 1);
 }
