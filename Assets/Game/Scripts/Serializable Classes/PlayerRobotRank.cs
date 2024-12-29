0ba121e2c90514b69d0ca569656b5684b4a51b95
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-29 12:45:06 +0100

    Make the player robot rank class as derived

--- Assets/Game/Scripts/Serializable Classes/PlayerRobotRank.cs
+++ Assets/Game/Scripts/Serializable Classes/PlayerRobotRank.cs
@@ -2,19 +2,9 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class PlayerRobotRank
+public class PlayerRobotRank : RobotRank
 {
-	[SerializeField, Min(1)] private int health;
-	[SerializeField, Min(1)] private int damage;
 	[SerializeField, Min(1)] private int bulletsLimitAtOnce;
-	[SerializeField, Min(0.01f)] private float movementSpeed;
-	[SerializeField, Min(0.01f)] private float bulletSpeed;
-	[SerializeField] private bool canDestroyMetal;
 
-	public int GetHealth() => health;
-	public int GetDamage() => damage;
 	public int GetBulletsLimitAtOnce() => bulletsLimitAtOnce;
-	public float GetMovementSpeed() => movementSpeed;
-	public float GetBulletSpeed() => bulletSpeed;
-	public bool CanDestroyMetal() => canDestroyMetal;
 }
