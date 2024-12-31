c248789278162f338a17a8d199fa68d993b62560
JasonNumberThirteen <jasonxiii97@gmail.com>
2024-12-31 03:14:08 +0100

    Add a script for the shield visual effect

---
+++ Assets/Game/Scripts/Scenes/Stage/ShieldVisualEffect.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Timer))]
+public class ShieldVisualEffect : MonoBehaviour
+{
+	private Timer timer;
+	
+	private void Awake()
+	{
+		timer = GetComponent<Timer>();
+
+		RegisterToListeners(true);
+	}
+
+	private void OnDestroy()
+	{
+		RegisterToListeners(false);
+	}
+
+	private void RegisterToListeners(bool register)
+	{
+		if(register)
+		{
+			timer.onReset.AddListener(OnTimerReset);
+			timer.onEnd.AddListener(OnTimerEnd);
+		}
+		else
+		{
+			timer.onReset.RemoveListener(OnTimerReset);
+			timer.onEnd.RemoveListener(OnTimerEnd);
+		}
+	}
+
+	private void OnTimerReset()
+	{
+		SetActive(true);
+	}
+
+	private void OnTimerEnd()
+	{
+		SetActive(false);
+	}
+
+	private void SetActive(bool active)
+	{
+		gameObject.SetActive(active);
+	}
+}
