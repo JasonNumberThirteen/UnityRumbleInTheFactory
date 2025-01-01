913ca62f6eb26d8e287d8fd5ea5cd10fac52d320
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-01 18:30:12 +0100

    Fix respawning player robot after being destroyed

--- Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Player Robot/PlayerRobot.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Player Robot/PlayerRobot.cs
@@ -1,4 +1,12 @@
 public class PlayerRobot : Robot
 {
 	public override bool IsFriendly() => true;
+
+	private void OnDestroy()
+	{
+		if(TryGetComponent(out PlayerRobotData prd))
+		{
+			prd.Data.Spawner.InitiateRespawn();
+		}
+	}
 }
