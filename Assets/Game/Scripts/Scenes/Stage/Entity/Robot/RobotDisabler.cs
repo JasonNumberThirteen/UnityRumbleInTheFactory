824ba641e8517052aa18f468ee0e8300122327ca
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-02 15:40:16 +0100

    Change implementation of getting behaviours in the robot disabler script

--- Assets/Game/Scripts/Scenes/Stage/Entity/Robot/RobotDisabler.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/RobotDisabler.cs
@@ -1,12 +1,28 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class RobotDisabler : MonoBehaviour
 {
-	public void SetComponentsActive(bool active)
+	private Behaviour[] behaviours;
+	
+	public void SetBehavioursActive(bool active)
 	{
-		GetComponents()?.ForEach(component => component.enabled = active);
+		if(behaviours == null)
+		{
+			return;
+		}
+		
+		foreach (var behaviour in behaviours)
+		{
+			behaviour.enabled = active;
+		}
 	}
 
-	protected abstract List<Behaviour> GetComponents();
+	protected abstract List<Behaviour> GetBehaviours();
+
+	private void Awake()
+	{
+		behaviours = GetBehaviours()?.Where(behaviour => behaviour.TryGetComponent(out Behaviour _)).ToArray();
+	}
 }
