3f9cd4d7ff03ab7d45f9c99cf97df9ae3823c1d7
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-02 17:17:17 +0100

    Refactor code in the player robot rank controller script

--- Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Player Robot/PlayerRobotRankController.cs
+++ Assets/Game/Scripts/Scenes/Stage/Entity/Robot/Player Robot/PlayerRobotRankController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerRobot))]
@@ -6,31 +7,78 @@ public class PlayerRobotRankController : MonoBehaviour
 	public PlayerRobotRank CurrentRank {get; private set;}
 
 	private PlayerRobot playerRobot;
+	private PlayersDataManager playersDataManager;
 	
 	public void Promote()
 	{
-		++playerRobot.GetPlayerData().RankNumber;
-
-		SetRank();
-	}
-	
-	public void SetRank()
-	{
-		CurrentRank = playerRobot.GetPlayerData().GetRank();
-
-		UpdateValues();
-	}
-
-	private void Awake() => playerRobot = GetComponent<PlayerRobot>();
-	private void Start() => SetRank();
-
-	private void UpdateValues()
-	{
-		IUpgradeableByRobotRank[] upgradeables = GetComponents<IUpgradeableByRobotRank>();
-
-		foreach (IUpgradeableByRobotRank upgradeable in upgradeables)
+		InvokeActionOnPlayerDataIfPossible(playerData =>
 		{
-			upgradeable.UpdateValuesUpgradeableByRobotRank(CurrentRank);
+			if(playersDataManager != null)
+			{
+				playersDataManager.ModifyRank(playerData, 1);
+			}
+		});
+	}
+
+	private void Awake()
+	{
+		playerRobot = GetComponent<PlayerRobot>();
+		playersDataManager = FindAnyObjectByType<PlayersDataManager>();
+
+		RegisterToListeners(true);
+	}
+
+	private void Start()
+	{
+		OnPlayerRankChanged();
+	}
+
+	private void OnDestroy()
+	{
+		RegisterToListeners(false);
+	}
+
+	private void RegisterToListeners(bool register)
+	{
+		if(register)
+		{
+			if(playersDataManager != null)
+			{
+				playersDataManager.playerRankChangedEvent.AddListener(OnPlayerRankChanged);
+			}
+		}
+		else
+		{
+			if(playersDataManager != null)
+			{
+				playersDataManager.playerRankChangedEvent.RemoveListener(OnPlayerRankChanged);
+			}
+		}
+	}
+
+	private void OnPlayerRankChanged()
+	{
+		InvokeActionOnPlayerDataIfPossible(playerData => CurrentRank = playerData.GetRank());
+		UpdateValuesUpgradeableByRobotRank();
+	}
+
+	private void UpdateValuesUpgradeableByRobotRank()
+	{
+		var upgradeableByRobotRankComponents = GetComponents<IUpgradeableByRobotRank>();
+
+		foreach (var upgradeableByRobotRank in upgradeableByRobotRankComponents)
+		{
+			upgradeableByRobotRank.UpdateValuesUpgradeableByRobotRank(CurrentRank);
+		}
+	}
+
+	private void InvokeActionOnPlayerDataIfPossible(Action<PlayerData> action)
+	{
+		var playerData = playerRobot.GetPlayerData();
+
+		if(playerData != null)
+		{
+			action?.Invoke(playerData);
 		}
 	}
 }
