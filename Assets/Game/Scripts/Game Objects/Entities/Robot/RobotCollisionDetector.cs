9aa6289b4cd982790d50627c787ad25390b6622f
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-04 21:44:43 +0100

    Refactor code in the robot collision detector script

--- Assets/Game/Scripts/Game Objects/Entities/Robot/RobotCollisionDetector.cs
+++ Assets/Game/Scripts/Game Objects/Entities/Robot/RobotCollisionDetector.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class RobotCollisionDetector : MonoBehaviour
 {
-	public LayerMask detectionLayers;
+	[SerializeField] private LayerMask layerMask;
 	
 	private Collider2D c2D;
 
-	public Collider2D OverlapBox() => Physics2D.OverlapBox(c2D.bounds.center, c2D.bounds.size, 0f, detectionLayers);
-	public Collider2D[] OverlapBoxAll() => Physics2D.OverlapBoxAll(c2D.bounds.center, c2D.bounds.size, 0f, detectionLayers);
+	public Collider2D OverlapBox() => Physics2D.OverlapBox(GetColliderCenter(), GetColliderSize(), 0f, layerMask);
+	public Collider2D[] OverlapBoxAll() => Physics2D.OverlapBoxAll(GetColliderCenter(), GetColliderSize(), 0f, layerMask);
 
-	private void Awake() => c2D = GetComponent<Collider2D>();
+	private Vector2 GetColliderCenter() => c2D.bounds.center;
+	private Vector2 GetColliderSize() => c2D.bounds.size;
+
+	private void Awake()
+	{
+		c2D = GetComponent<Collider2D>();
+	}
 }
