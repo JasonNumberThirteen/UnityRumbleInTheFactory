a533017d1a16aad2a0f67fe4b6a991a4c1d6007c
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-04 16:46:28 +0100

    Rename script for the life bonus trigger events receiver

--- Assets/Game/Scripts/Trigger Events/Receivers/Bonus/One Time/LifeBonusTriggerEventsReceiver.cs
+++ Assets/Game/Scripts/Trigger Events/Receivers/Bonus/One Time/LifeBonusTriggerEventsReceiver.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class LifeBonusTrigger : BonusTriggerEventsReceiver
+public class LifeBonusTriggerEventsReceiver : BonusTriggerEventsReceiver
 {
 	public override void TriggerOnEnter(GameObject sender)
 	{
