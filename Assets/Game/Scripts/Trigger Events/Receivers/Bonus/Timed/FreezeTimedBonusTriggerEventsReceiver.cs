89fccc1490cdc62255637d56179c39d396c2589e
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-04 17:03:05 +0100

    Rename script for the freeze timed bonus trigger events receiver

--- Assets/Game/Scripts/Trigger Events/Receivers/Bonus/Timed/FreezeTimedBonusTriggerEventsReceiver.cs
+++ Assets/Game/Scripts/Trigger Events/Receivers/Bonus/Timed/FreezeTimedBonusTriggerEventsReceiver.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class FreezeBonusTrigger : TimedBonusTriggerEventsReceiver
+public class FreezeTimedBonusTriggerEventsReceiver : TimedBonusTriggerEventsReceiver
 {
 	private RobotDisablingManager robotDisablingManager;
 	
