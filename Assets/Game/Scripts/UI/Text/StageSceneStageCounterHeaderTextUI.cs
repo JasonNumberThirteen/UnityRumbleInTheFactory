efe30a3d6aae236572f01db94d31d6ffbd65e933
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-06 12:50:02 +0100

    Add a script for the stage scene stage counter header text UI

---
+++ Assets/Game/Scripts/UI/Text/StageSceneStageCounterHeaderTextUI.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class StageSceneStageCounterHeaderTextUI : StageCounterHeaderTextUI
+{
+	private TranslationBackgroundPanelUI translationBackgroundPanelUI;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		
+		translationBackgroundPanelUI = FindAnyObjectByType<TranslationBackgroundPanelUI>(FindObjectsInactive.Include);
+
+		RegisterToListeners(true);
+	}
+
+	private void OnDestroy()
+	{
+		RegisterToListeners(false);
+	}
+
+	private void RegisterToListeners(bool register)
+	{
+		if(register)
+		{
+			if(translationBackgroundPanelUI != null)
+			{
+				translationBackgroundPanelUI.panelStartedTranslationEvent.AddListener(OnPanelStartedTranslation);
+			}
+		}
+		else
+		{
+			if(translationBackgroundPanelUI != null)
+			{
+				translationBackgroundPanelUI.panelStartedTranslationEvent.RemoveListener(OnPanelStartedTranslation);
+			}
+		}
+	}
+
+	private void OnPanelStartedTranslation()
+	{
+		gameObject.SetActive(false);
+	}
+}
