4bcad5bf7b827ebf0c2a2dc4afd8fcf03200e1ee
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-06 12:42:37 +0100

    Deactivate the difficulty tier panel UI when the translation background panel UI starts translating

--- Assets/Game/Scripts/UI/Panel/DifficultyTierPanelUI.cs
+++ Assets/Game/Scripts/UI/Panel/DifficultyTierPanelUI.cs
@@ -5,6 +5,15 @@ public class DifficultyTierPanelUI : MonoBehaviour
 	[SerializeField] private GameData gameData;
 	[SerializeField] private GameObject difficultyTierImageUIPrefab;
 
+	private TranslationBackgroundPanelUI translationBackgroundPanelUI;
+
+	private void Awake()
+	{
+		translationBackgroundPanelUI = FindAnyObjectByType<TranslationBackgroundPanelUI>(FindObjectsInactive.Include);
+
+		RegisterToListeners(true);
+	}
+
 	private void Start()
 	{
 		if(difficultyTierImageUIPrefab == null)
@@ -18,5 +27,33 @@ public class DifficultyTierPanelUI : MonoBehaviour
 		{
 			Instantiate(difficultyTierImageUIPrefab, gameObject.transform);
 		}
+	}
+
+	private void OnDestroy()
+	{
+		RegisterToListeners(false);
+	}
+
+	private void RegisterToListeners(bool register)
+	{
+		if(register)
+		{
+			if(translationBackgroundPanelUI != null)
+			{
+				translationBackgroundPanelUI.panelStartedTranslationEvent.AddListener(OnPanelStartedTranslation);
+			}
+		}
+		else
+		{
+			if(translationBackgroundPanelUI != null)
+			{
+				translationBackgroundPanelUI.panelStartedTranslationEvent.RemoveListener(OnPanelStartedTranslation);
+			}
+		}
+	}
+
+	private void OnPanelStartedTranslation()
+	{
+		gameObject.SetActive(false);
 	}
 }
