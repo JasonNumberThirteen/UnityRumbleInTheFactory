bf733cc9dd2146626a83ced8bc52e6ec015129ce
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-07 00:27:34 +0100

    Change order of directives in the enemy robot entity movement direction selector script

--- Assets/Game/Scripts/Game Objects/Entities/Robot/Enemy Robot/EnemyRobotEntityMovementDirectionSelector.cs
+++ Assets/Game/Scripts/Game Objects/Entities/Robot/Enemy Robot/EnemyRobotEntityMovementDirectionSelector.cs
@@ -1,7 +1,7 @@
 using Random = UnityEngine.Random;
-using UnityEngine;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class EnemyRobotEntityMovementDirectionSelector : MonoBehaviour
 {
