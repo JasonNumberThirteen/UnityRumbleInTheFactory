1e24dda15f82e312efb945fa32551fcb27549f33
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-09 13:49:10 +0100

    Add a lock to the bullet trigger events sender to trigger only once

--- Assets/Game/Scripts/Trigger Events/Senders/BulletTriggerEventsSender.cs
+++ Assets/Game/Scripts/Trigger Events/Senders/BulletTriggerEventsSender.cs
@@ -4,9 +4,16 @@ public class BulletTriggerEventsSender : MonoBehaviour
 {
 	[SerializeField] private GameObject splatterEffectPrefab;
 	[SerializeField] private string[] ignoredTags;
+
+	private bool triggered;
 	
 	private void OnTriggerEnter2D(Collider2D collider)
 	{
+		if(triggered)
+		{
+			return;
+		}
+		
 		foreach (var ignoredTag in ignoredTags)
 		{
 			if(collider.CompareTag(ignoredTag))
@@ -14,6 +21,8 @@ public class BulletTriggerEventsSender : MonoBehaviour
 				return;
 			}
 		}
+
+		triggered = true;
 
 		SendTriggerOnEnter(collider);
 		SpawnSplatterEffect();
