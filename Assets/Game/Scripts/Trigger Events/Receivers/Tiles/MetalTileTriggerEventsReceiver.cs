2094a6d8cadb6b0582f6d43289aa5f5b0f387063
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-09 13:53:16 +0100

    Play sound when the player bullet hit metal tile

--- Assets/Game/Scripts/Trigger Events/Receivers/Tiles/MetalTileTriggerEventsReceiver.cs
+++ Assets/Game/Scripts/Trigger Events/Receivers/Tiles/MetalTileTriggerEventsReceiver.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class MetalTileTriggerEventsReceiver : MonoBehaviour, ITriggerableOnEnter
 {
+	private StageSoundManager stageSoundManager;
+	
 	public void TriggerOnEnter(GameObject sender)
 	{
 		if(CanBeDestroyedByBullet(sender))
 		{
 			Destroy(gameObject);
 		}
+		else if(stageSoundManager != null && sender != null && sender.TryGetComponent(out PlayerRobotEntityBulletEntity _))
+		{
+			stageSoundManager.PlaySound(SoundEffectType.PlayerRobotBulletHit);
+		}
+	}
+
+	private void Awake()
+	{
+		stageSoundManager = FindAnyObjectByType<StageSoundManager>();
 	}
 
 	private bool CanBeDestroyedByBullet(GameObject sender) => sender.TryGetComponent(out BulletEntity bulletEntity) && bulletEntity.CanDestroyMetal();
