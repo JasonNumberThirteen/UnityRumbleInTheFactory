1110615be900dc1f91d8faa6c2d33d49578be669
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-10 18:32:55 +0100

    Change appearance of the player robot by its current rank

---
+++ Assets/Game/Scripts/Animator Controllers/PlayerRobotEntityAnimatorController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerRobotEntityRankController))]
+public class PlayerRobotEntityAnimatorController : RobotEntityAnimatorController
+{
+	private PlayerRobotEntityRankController playerRobotEntityRankController;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		playerRobotEntityRankController = GetComponent<PlayerRobotEntityRankController>();
+
+		RegisterToListeners(true);
+	}
+
+	private void OnDestroy()
+	{
+		RegisterToListeners(false);
+	}
+
+	private void RegisterToListeners(bool register)
+	{
+		if(register)
+		{
+			playerRobotEntityRankController.rankChangedEvent.AddListener(OnRankChanged);
+		}
+		else
+		{
+			playerRobotEntityRankController.rankChangedEvent.RemoveListener(OnRankChanged);
+		}
+	}
+
+	private void OnRankChanged(PlayerRobotRank playerRobotRank)
+	{
+		animator.runtimeAnimatorController = playerRobotRank.GetRuntimeAnimatorController();
+	}
+}
