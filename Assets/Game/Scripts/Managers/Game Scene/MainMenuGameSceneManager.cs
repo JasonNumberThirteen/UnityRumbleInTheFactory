02befea5dd2ab6248ffae2de776d74b8fe88e98f
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-01-14 12:47:51 +0100

    Remove unused directives

--- Assets/Game/Scripts/Managers/Game Scene/MainMenuGameSceneManager.cs
+++ Assets/Game/Scripts/Managers/Game Scene/MainMenuGameSceneManager.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 public class MainMenuGameSceneManager : GameSceneManager
 {
 	private TranslationBackgroundPanelUI translationBackgroundPanelUI;
--- Assets/Game/Scripts/Managers/Option/MainMenuOptionSelectionManager.cs
+++ Assets/Game/Scripts/Managers/Option/MainMenuOptionSelectionManager.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Runtime.Serialization.Formatters;
 using UnityEngine;
 
 [RequireComponent(typeof(LoopingIntCounter))]
