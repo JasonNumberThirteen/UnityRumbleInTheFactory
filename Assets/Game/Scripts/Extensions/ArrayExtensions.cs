53cc8242351367957a70124d0811ef1b98b0376e
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-02-13 12:54:24 +0100

    Remove null checks from extension methods for arrays and lists

--- Assets/Game/Scripts/Extensions/ArrayExtensions.cs
+++ Assets/Game/Scripts/Extensions/ArrayExtensions.cs
@@ -3,15 +3,10 @@ using System;
 
 public static class ArrayExtensions
 {
-	public static E GetRandomElement<E>(this E[] array) => array != null && array.Length > 0 ? array[Random.Range(0, array.Length)] : default;
+	public static E GetRandomElement<E>(this E[] array) => array.Length > 0 ? array[Random.Range(0, array.Length)] : default;
 	
 	public static void ForEach<E>(this E[] array, Action<E> action)
 	{
-		if(array == null)
-		{
-			return;
-		}
-		
 		foreach (var element in array)
 		{
 			action?.Invoke(element);
--- Assets/Game/Scripts/Extensions/ListExtensions.cs
+++ Assets/Game/Scripts/Extensions/ListExtensions.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public static class ListExtensions
 {
-	public static E GetRandomElement<E>(this List<E> list) => list != null && list.Count > 0 ? list[Random.Range(0, list.Count)] : default;
+	public static E GetRandomElement<E>(this List<E> list) => list.Count > 0 ? list[Random.Range(0, list.Count)] : default;
 	
 	public static void ForEachIndexed<E>(this List<E> list, Action<E, int> action, int counterInitialValue = 0)
 	{
