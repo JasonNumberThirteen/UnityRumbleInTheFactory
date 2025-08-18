6b4c8acd5a1bd13cc7fda024e202220dc0744631
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-08-18 22:16:11 +0200

    Remove unused method from the robot data methods script

--- Assets/Game/Scripts/Static Methods/RobotDataMethods.cs
+++ Assets/Game/Scripts/Static Methods/RobotDataMethods.cs
@@ -1,5 +1,4 @@
 public static class RobotDataMethods
 {
-	public static bool RobotDataHasOrdinalNumber(RobotData robotData, int ordinalNumber) => GetOrdinalNumber(robotData) == ordinalNumber;
 	public static int GetOrdinalNumber(RobotData robotData) => robotData != null ? robotData.GetOrdinalNumber() : -1;
 }
