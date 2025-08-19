e4d9b0aeafac272195f86b6b003d5ee16e7c6a00
JasonNumberThirteen <jasonxiii97@gmail.com>
2025-08-19 18:30:43 +0200

    Replace concatenation with a string interpolation in the prefixed integer counter text UI script

--- Assets/Game/Scripts/UI/Text/Int Counter/Prefixed/PrefixedIntCounterTextUI.cs
+++ Assets/Game/Scripts/UI/Text/Int Counter/Prefixed/PrefixedIntCounterTextUI.cs
@@ -4,7 +4,7 @@ public class PrefixedIntCounterTextUI : IntCounterTextUI
 {
 	[SerializeField, Min(0)] private int width;
 
-	public override string GetCounterValueAsString() => string.Format("{0," + width + "}", GetFormattedCounterValueAsString());
+	public override string GetCounterValueAsString() => string.Format($"{{0,{width}}}", GetFormattedCounterValueAsString());
 
 	public virtual string GetFormattedCounterValueAsString() => base.GetCounterValueAsString();
 
